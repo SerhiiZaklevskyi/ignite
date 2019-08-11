4554915d763e59f13ce3093cd4d221386ee430f3
SerhiiZaklevskyi <dasdhas1234@gmail.com>
2019-08-11 12:31:45 +0300

    lifted state of Music Search component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,11 +6,57 @@ import Footer from "../Footer/Footer.js";
 import MyPlaylists from "../MyPlaylists/MyPlaylists";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playlistTracks: [],
+      searchTracks: []
+    };
+
+    this.search = this.search.bind(this);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+  search(inputValue) {
+    fetch("./songs.json", {
+      cache: "no-store"
+    })
+      .then(res => res.json())
+      .then(data =>
+        this.setState({
+          searchTracks: data.filter(
+            currentTrack => currentTrack.name === inputValue
+          )
+        })
+      );
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({playlistTracks: tracks});
+  }
   render() {
     return (
       <div className={styles.App}>
         <Header />
-        <MusicSearch />
+        <MusicSearch
+          onSearch={this.search}
+          searchResults={this.state.searchTracks}
+          onAdd={this.addTrack}
+          playlistTracks={this.state.playlistTracks}
+          onRemove={this.removeTrack}
+        />
         <MyPlaylists />
         <Footer />
       </div>
--- src/components/MusicSearch/MusicSearch.js
+++ src/components/MusicSearch/MusicSearch.js
@@ -6,59 +6,18 @@ import SearchResults from "./SearchResults/SearchResults";
 import PlayList from "./PlayList/PlayList";
 
 class MusicSearch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      playlistTracks: [],
-      searchTracks: []
-    };
-
-    this.search = this.search.bind(this);
-    this.addTrack = this.addTrack.bind(this);
-    this.removeTrack = this.removeTrack.bind(this);
-  }
-  search(inputValue) {
-    fetch("./songs.json", {
-      cache: "no-store"
-    })
-      .then(res => res.json())
-      .then(data =>
-        this.setState({
-          searchTracks: data.filter(
-            currentTrack => currentTrack.name === inputValue
-          )
-        })
-      );
-  }
-
-  addTrack(track) {
-    let tracks = this.state.playlistTracks;
-    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
-      return;
-    }
-    tracks.push(track);
-    this.setState({playlistTracks: tracks});
-    console.log(tracks);
-  }
-
-  removeTrack(track) {
-    let tracks = this.state.playlistTracks;
-    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
-
-    this.setState({playlistTracks: tracks});
-  }
   render() {
     return (
       <div>
-        <SearchBar onSearch={this.search} />
+        <SearchBar onSearch={this.props.onSearch} />
         <div className={styles.wrapper}>
           <SearchResults
-            searchResults={this.state.searchTracks}
-            onAdd={this.addTrack}
+            searchResults={this.props.searchResults}
+            onAdd={this.props.onAdd}
           />
           <PlayList
-            playlistTracks={this.state.playlistTracks}
-            onRemove={this.removeTrack}
+            playlistTracks={this.props.playlistTracks}
+            onRemove={this.props.onRemove}
           />
         </div>
       </div>
