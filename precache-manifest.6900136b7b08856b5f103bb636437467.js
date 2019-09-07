d638b873fad2178eefdf5456964db640cb0e8ac8
SerhiiZaklevskyi <dasdhas1234@gmail.com>
2019-09-07 14:59:44 +0300

    Updates

---
+++ precache-manifest.6900136b7b08856b5f103bb636437467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c38d2ced55da5a7ed50ea514034133",
+    "url": "/ignite/index.html"
+  },
+  {
+    "revision": "5c67bdbeb104b5b5b8d1",
+    "url": "/ignite/static/css/main.e070beb3.chunk.css"
+  },
+  {
+    "revision": "1d0de86e26283e5dc58a",
+    "url": "/ignite/static/js/2.d19b1d5f.chunk.js"
+  },
+  {
+    "revision": "5c67bdbeb104b5b5b8d1",
+    "url": "/ignite/static/js/main.7120311b.chunk.js"
+  },
+  {
+    "revision": "8176aada5eb66e435d5f",
+    "url": "/ignite/static/js/runtime~main.7aa35dbc.js"
+  },
+  {
+    "revision": "a6b484d1039aa82ae8067604ced4f826",
+    "url": "/ignite/static/media/319256.a6b484d1.jpg"
+  }
+]);
